39a5293fad7530a77f460552b3287642cae0392c
Drona Suryawanshi <dronasuryawanshi970@gmail.com>
2026-06-23 18:09:22 +0530

    Time: 3 ms (58.53%), Space: 43.7 MB (41.4%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+         StringBuilder sb = new StringBuilder();
+        int balance = 0;
+int n = s.length();
+        for(int i = 0; i < n; i++){
+            if(s.charAt(i) == '('){
+                if(balance > 0){
+                    sb.append(s.charAt(i));
+                }
+                balance++;
+            } else {
+                balance--;
+                if(balance > 0){
+                    sb.append(s.charAt(i));
+                }
+            }
+        }
+        return(sb.toString());
+    }
+}
